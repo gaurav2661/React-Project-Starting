ba7c40212be53725480cdada14b0c36f1ea5dbdb
Gaurav Singh <gaurav.2661@gmail.com>
2024-08-11 18:52:37 +0530

    showing dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
 
-
+const reactDescription = ["Fundamental","Crucial","Core"]
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
 function Header() {
     return <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
         <h1>React Essentials</h1>
-        <p>Fundamental react concepta you will need for almost any app you are going to build</p>
+        <p>
+            {reactDescription[getRandomInt(2)]} react concepta you will need for almost any app you are going to build
+        </p>
     </header>
 }
 
 function App() {
-
     return (
         <div>
             <Header></Header>
