b7339014ba4288bac17a04cbd5ab8fb4fb6a61bd
Gaurav Singh <gaurav.2661@gmail.com>
2024-08-11 18:53:35 +0530

    showing dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
 function Header() {
+    const description = reactDescription[getRandomInt(2)];
     return <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
         <h1>React Essentials</h1>
         <p>
-            {reactDescription[getRandomInt(2)]} react concepta you will need for almost any app you are going to build
+            {description} react concepta you will need for almost any app you are going to build
         </p>
     </header>
 }
