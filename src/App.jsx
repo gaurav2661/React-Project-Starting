5d0cc26b13c84850e8a2353d1ea2cf63cf2c5dc8
Gaurav Singh <gaurav.2661@gmail.com>
2024-08-15 15:24:26 +0530

    data.js updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 import {useState} from "react";
+import {EXAMPLES} from "./data.js";
 
 function App() {
 
-    const [selectedTopic,setSelectedTopic] = useState('Please click a button');
+    const [selectedTopic,setSelectedTopic] = useState('Components');
     function handleSelect(selectedButton) {
         setSelectedTopic( selectedButton)
         console.log(selectedButton)
@@ -34,7 +35,15 @@ function App() {
                         <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
                         <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
                     </menu>
-                    {selectedTopic}
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>
+                              {EXAMPLES[selectedTopic].code}
+                            </code>
+                        </pre>
+                    </div>
                 </section>
             </main>
         </div>
