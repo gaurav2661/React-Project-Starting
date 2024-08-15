56488fec8f7a594f3298013b80420d4a2fe3ceef
Gaurav Singh <gaurav.2661@gmail.com>
2024-08-15 11:46:51 +0530

    importing image file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import reactImg from './assets/react-core-concepts.png'
 
 const reactDescription = ["Fundamental","Crucial","Core"]
 function getRandomInt(max) {
@@ -7,7 +8,7 @@ function getRandomInt(max) {
 function Header() {
     const description = reactDescription[getRandomInt(2)];
     return <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+        <img src={reactImg} alt="Stylized atom"/>
         <h1>React Essentials</h1>
         <p>
             {description} react concepta you will need for almost any app you are going to build
