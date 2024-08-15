d530d5b297feb4debb0e410dd6fe0f9583ae8195
Gaurav Singh <gaurav.2661@gmail.com>
2024-08-15 13:47:39 +0530

    listen clicks

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+    function handleSelect() {
+        console.log("On selected")
+    }
     return (
         <div>
             <Header></Header>
@@ -21,11 +24,12 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
+                        <TabButton onSelect={handleSelect}>Components</TabButton>
+                        <TabButton onSelect={handleSelect}>JSX</TabButton>
+                        <TabButton onSelect={handleSelect}>Props</TabButton>
+                        <TabButton onSelect={handleSelect}>State</TabButton>
                     </menu>
+                    Dynamic Content
                 </section>
             </main>
         </div>
