c11a3633af673fed1354799eaa8634da33447a82
Gaurav Singh <gaurav.2661@gmail.com>
2024-08-17 14:13:38 +0530

    Outputting List Data Dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,12 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept {...CORE_CONCEPTS[0]}/>
-                        <CoreConcept {...CORE_CONCEPTS[1]}/>
-                        <CoreConcept {...CORE_CONCEPTS[2]}/>
-                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                        {/* eslint-disable-next-line react/jsx-key */}
+                        {CORE_CONCEPTS.map((coreItem) => <CoreConcept{...coreItem}/>)}
+                        {/*<CoreConcept {...CORE_CONCEPTS[0]}/>*/}
+                        {/*<CoreConcept {...CORE_CONCEPTS[1]}/>*/}
+                        {/*<CoreConcept {...CORE_CONCEPTS[2]}/>*/}
+                        {/*<CoreConcept {...CORE_CONCEPTS[3]}/>*/}
                     </ul>
                 </section>
                 <section id="examples">
